0c28669bdca33df8d80ec1fe2eeb1ccf8ed95e95
kimyujin <jjks20007@daum.net>
2023-01-06 18:21:00 +0900

    2023.01.06

--- ex05/src/main/java/org/zerock/controller/UploadController.java
+++ ex05/src/main/java/org/zerock/controller/UploadController.java
@@ -1,14 +1,30 @@
 package org.zerock.controller;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.domain.AttachFileDTO;
 
 import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
 
 @Controller
 @Log4j
@@ -18,24 +34,24 @@ public class UploadController {
 		log.info("upload form");
 	}
 	
-	@PostMapping("/uploadFormAction")
-	public void uploadFormPost(MultipartFile[] uploadFile, Model model) {
-		
-		String uploadFolder = "C:\\upload";
-		
-		for(MultipartFile multipartFile : uploadFile) {
-			log.info("------------------------------------");
-			log.info("Upload File Name: " + multipartFile.getOriginalFilename()); // 실제 pc에 저장되어있는 이름
-			log.info("Upload File Size: " + multipartFile.getSize()); // 업로드한 파일의 크기
-			
-			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
-			try {
-				multipartFile.transferTo(saveFile);
-			} catch (Exception e) {
-				log.error(e.getMessage());
-			}
-		}
-	}
+//	@PostMapping("/uploadFormAction")
+//	public void uploadFormPost(MultipartFile[] uploadFile, Model model) {
+//		
+//		String uploadFolder = "C:\\upload";
+//		
+//		for(MultipartFile multipartFile : uploadFile) {
+//			log.info("------------------------------------");
+//			log.info("Upload File Name: " + multipartFile.getOriginalFilename()); // 실제 pc에 저장되어있는 이름
+//			log.info("Upload File Size: " + multipartFile.getSize()); // 업로드한 파일의 크기
+//			
+//			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+//			try {
+//				multipartFile.transferTo(saveFile);
+//			} catch (Exception e) {
+//				log.error(e.getMessage());
+//			}
+//		}
+//	}
 	
 	
 	// Ajax를 이용한 파일 업로드
@@ -44,13 +60,123 @@ public class UploadController {
 		log.info("upload ajax");
 	}
 	
-	@PostMapping("/uploadAjaxAction")
-	public void uploadAjaxPost(MultipartFile[] uploadFile) {
+	
+//	@PostMapping("/uploadAjaxAction") 
+//	public void uploadAjaxPost(MultipartFile[] uploadFile) {
+//		log.info("update ajax post...........");
+//		
+//		String uploadFolder = "C:\\upload";
+//		
+//		for(MultipartFile multipartFile : uploadFile) {
+//			log.info("------------------------------------");
+//			log.info("Upload File Name: " + multipartFile.getOriginalFilename()); // 실제 pc에 저장되어있는 이름
+//			log.info("Upload File Size: " + multipartFile.getSize()); // 업로드한 파일의 크기
+//			
+//			String uploadFileName = multipartFile.getOriginalFilename();
+//			
+//			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
+//			log.info("only file name: " + uploadFileName);
+//			
+//			File saveFile = new File(uploadFolder, uploadFileName);
+//
+//			try {
+//				multipartFile.transferTo(saveFile);
+//			} catch (Exception e) {
+//				log.error(e.getMessage());
+//			}
+//		}
+//	}
+	
+	// 년/월/일 폴더의 생성
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Date date = new Date();
+		String str = sdf.format(date);
+		return str.replace("-", File.separator); // 2023\01\06 
+	}
+	
+	
+	// 이미지 파일의 판단
+	private boolean checkImageType(File file) {
+		try {
+			String contentType = Files.probeContentType(file.toPath());
+			
+			// mime type
+			// audio/mpeg, text/xml, text/plain, image/jpeg
+			
+			return contentType.startsWith("image");
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	
+//	@PostMapping("/uploadAjaxAction") 
+//	public void uploadAjaxPost(MultipartFile[] uploadFile) {
+//		log.info("update ajax post...........");
+//		
+//		String uploadFolder = "C:\\upload";
+//		
+//		// make folder ----------------------------------------
+//		File uploadPath = new File(uploadFolder, getFolder());
+//		log.info("upload path:" + uploadPath);
+//		
+//		if(uploadPath.exists() == false) { // 해당 위치에 같은 이름을 가진 폴더가 있는지 확인 후
+//			uploadPath.mkdirs(); // 폴더를 생성한다.
+//		}
+//		
+//		for(MultipartFile multipartFile : uploadFile) {
+//			log.info("------------------------------------");
+//			log.info("Upload File Name: " + multipartFile.getOriginalFilename()); // 실제 pc에 저장되어있는 이름
+//			log.info("Upload File Size: " + multipartFile.getSize()); // 업로드한 파일의 크기
+//			
+//			String uploadFileName = multipartFile.getOriginalFilename();
+//			
+//			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
+//			log.info("only file name: " + uploadFileName);
+//			
+//			// File saveFile = new File(uploadFolder, uploadFileName);
+//			
+//			UUID uuid = UUID.randomUUID();
+//			
+//			uploadFileName = uuid.toString() + "_" + uploadFileName;
+//			
+//			File saveFile = new File(uploadPath, uploadFileName);
+//			
+//			
+//			try {
+//				multipartFile.transferTo(saveFile);
+//			} catch (Exception e) {
+//				log.error(e.getMessage());
+//			}
+//		}
+//	}
+	
+	
+	@PostMapping(value = "/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_UTF8_VALUE) 
+	@ResponseBody
+	public ResponseEntity<List<AttachFileDTO>> uploadAjaxPost(MultipartFile[] uploadFile) {
 		log.info("update ajax post...........");
 		
+		List<AttachFileDTO> list = new ArrayList<>();
 		String uploadFolder = "C:\\upload";
+		String uploadFolderPath = getFolder();
+		
+		// make folder ----------------------------------------
+		File uploadPath = new File(uploadFolder, uploadFolderPath);
+		log.info("upload path:" + uploadFolderPath);
+		
+		if(uploadPath.exists() == false) { // 해당 위치에 같은 이름을 가진 폴더가 있는지 확인 후
+			uploadPath.mkdirs(); // 폴더를 생성한다.
+		}
 		
 		for(MultipartFile multipartFile : uploadFile) {
+			
+			AttachFileDTO attachDTO = new AttachFileDTO();
 			log.info("------------------------------------");
 			log.info("Upload File Name: " + multipartFile.getOriginalFilename()); // 실제 pc에 저장되어있는 이름
 			log.info("Upload File Size: " + multipartFile.getSize()); // 업로드한 파일의 크기
@@ -60,16 +186,56 @@ public class UploadController {
 			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
 			log.info("only file name: " + uploadFileName);
 			
-			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+			attachDTO.setFileName(uploadFileName);
+			
+			// File saveFile = new File(uploadFolder, uploadFileName);
+			
+			UUID uuid = UUID.randomUUID();
+			
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+	
 			try {
+				File saveFile = new File(uploadPath, uploadFileName);
 				multipartFile.transferTo(saveFile);
+				
+				attachDTO.setUuid(uuid.toString());
+				attachDTO.setUploadPath(uploadFolderPath);
+				
+				//check image type file
+				if (checkImageType(saveFile)) {
+					
+					attachDTO.setImage(true);
+					FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
+					thumbnail.close();
+				}
+				
+				list.add(attachDTO);
+				
 			} catch (Exception e) {
 				log.error(e.getMessage());
 			}
 		}
+		return new ResponseEntity<List<AttachFileDTO>>(list, HttpStatus.OK);
 	}
 	
-	
+	// 섬네일 데이터 전송하기
+	@GetMapping("/display")
+	@ResponseBody
+	public ResponseEntity<byte[]> getFile(String fileName){
+		log.info("fileName :" + fileName);
+		File file = new File("c:\\upload\\" + fileName);
+		log.info("file: " + file);
+		ResponseEntity<byte[]> result = null;
+		try {
+			HttpHeaders header = new HttpHeaders();
+			header.add("Content-Type", Files.probeContentType(file.toPath()));
+			result = new ResponseEntity<byte[]>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
 	
 	
 	
--- ex05/src/main/webapp/WEB-INF/views/uploadAjax.jsp
+++ ex05/src/main/webapp/WEB-INF/views/uploadAjax.jsp
@@ -5,20 +5,92 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<style>
+.uploadResult {
+   width: 100%;
+   background-color: gray;
+}
+
+.uploadResult ul {
+   display: flex;
+   flex-flow: row;
+   justify-content: center;
+   align-items: center;
+}
+
+.uploadResult ul li {
+   list-style: none;
+   padding: 10px;
+}
+
+.uploadResult ul li img {
+   width: 100px;
+}
+</style>
+
+<style>
+.bigPictureWrapper {
+  position: absolute;
+  display: none;
+  justify-content: center;
+  align-items: center;
+  top:0%;
+  width:100%;
+  height:100%;
+  background-color: gray; 
+  z-index: 100;
+}
+
+.bigPicture {
+  position: relative;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
 </head>
 <body>
+	
 	<h1>Upload with Ajax</h1>
 
 	<div class='uploadDiv'>
 		<input type='file' name='uploadFile' multiple>
 	</div>
+	
+	<div class='bigPictureWrapper'>
+		<div class='bigPicture'></div>
+	</div>
+
+	<div class='uploadResult'>
+		<ul>
+			
+		</ul>
+	</div>
 
 	<button id='uploadBtn'>Upload</button>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 <script type="text/javascript">
 	$(document).ready(function() {
+		var regex = new RegExp("(.*?)\.(exe|sh|zip|alz)$");
+		var maxSize = 5242880;
+		
+		function checkExtension(fileName, fileSize){
+			if(fileSize >= maxSize){
+				alert("파일 사이즈 초과");
+				return false;
+			}
+			
+			if(regex.test(fileName)){
+				alert("해당 종류의 파일은 업로드할 수 없습니다.");
+				return false;
+			}
+			return true;
+		}
+		
+		var cloneObj = $(".uploadDiv").clone();
+		
 		$("#uploadBtn").on("click", function(e) {
 		// jquery를 이용하는 경우에 파일 업로드는 FormData라는 객체를 이용하게 된다.
 		// FormData는 쉽게 말해서 가상의 <form>태그와 같다고 생각하면 된다.
@@ -31,6 +103,11 @@
 		console.log(files);
 		
 		for(var i = 0; i <files.length; i++){
+			
+			if(!checkExtension(files[i].name, files[i].size)){
+				return false;
+			}
+			
 			formData.append("uploadFile", files[i]);
 		}
 		
@@ -40,11 +117,34 @@
 			contentType: false,
 			data: formData,
 			type: 'POST',
+			dataType: 'json',
 			success: function(result){
-				alert("Uploaded");
+				console.log(result);
+				
+				showUploadedFile(result);
+				
+				$(".uploadDiv").html(cloneObj.html());
 			}
 		});
 	});
+		
+	var uploadResult = $(".uploadResult ul");
+	
+	function showUploadedFile(uploadResultArr){
+		
+		var str = "";
+		
+		$(uploadResultArr).each(function(i, obj){
+			
+			if(!obj.image){
+				str +="<li><img src='/resources/img/attach.png'>" + obj.fileName + "</li>";
+			} else {
+				str += "<li>" + obj.fileName + "</li>";
+			}
+		});
+		
+		uploadResult.append(str);
+	}
 });
 </script>
 
